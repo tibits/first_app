ff46bf7e7072b269395127cf89efb5f0ff8ba6e6
LBS <LBS@LBS.local>
2012-06-24 19:29:19 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e62545a7ecb6ff326a6cabe4cc31cf82612e3059764dd51e6333d46b95d11c232605293f55ecc5185db628018234d79bc7badaca69a65a26f0eb33135cee6332'
